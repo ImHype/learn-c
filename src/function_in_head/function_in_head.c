dc34f14a64aa4be00363918d9e0928fd47ae73bd
ImHype <543717080@qq.com>
2019-09-16 17:26:20 +0800

    chore: function in head file

---
+++ src/function_in_head/function_in_head.c
@@ -0,0 +1,7 @@
+#include "function_in_head.h"
+
+int main(int argc, char const *argv[])
+{
+    function_hello();
+    return 0;
+}
---
+++ src/function_in_head/function_in_head.h
@@ -0,0 +1,6 @@
+#include "stdio.h"
+
+int function_hello() {
+    printf("hello");
+    return 0;
+}
